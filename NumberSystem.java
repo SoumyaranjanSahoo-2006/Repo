924c58ab86b07e65358144c2f5fa43cd90ce29c9
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2025-12-21 23:41:14 +0530

    added

---
+++ NumberSystem.java
@@ -0,0 +1,30 @@
+public class NumberSystem {
+// Print numbers from 1 to N
+//  Input: 5 → Output: 1 2 3 4 5
+
+	public static void main(String...somu) {
+		int n;
+		for(n=1;n<=5;n++) {
+			System.out.println(n);
+		}
+	}
+
+}
+
+// Print numbers reverse from N to 1
+// Ex-Input: 5 → Output: 5 4 3 2 1
+
+ class Number {
+	public static void main(String...somu) {
+		
+		for(int n=15;n>=1;n--) {
+			System.out.println(n);
+		}
+	}
+
+
+}
+
+
+
+// Updated on 21 Dec
